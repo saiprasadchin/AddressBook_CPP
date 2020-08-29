cbe55c964545bd91dc9561dae9a0272b0e098ea4
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-08-29 07:35:08 -0700

    Added : Welcome message

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+void displayWelcomeMessage() {
+    cout << "Welcome to Address Book Program" << endl;
+}
+
+int main() {
+    displayWelcomeMessage();
+    return 0;
+}
